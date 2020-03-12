c5df7aac328bf52dcbb21bee4d266afa1a1bd14e
Tal Hayut <talhayut.dev@gmail.com>
2020-03-12 09:51:07 +0200

    trying export function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-export { default as Swype } from './components/Swype'
+import React from 'react';
+import SwypeComponent from './components/Swype';
+
+function Swype() {
+    return SwypeComponent;
+}
+
+export default Swype;
